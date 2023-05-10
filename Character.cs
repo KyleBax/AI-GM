26ae12f337234e9dcbed92dfa77642b0a74adf79
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-05-11 11:34:27 +1200

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,59 @@
+﻿namespace AI_GM
+{
+    /// <summary>
+    /// Holds the information of the characters
+    /// </summary>
+    internal class Character
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _strength;
+        public int Strength
+        {
+            get { return _strength; }
+            set { _strength = value; }
+        }
+
+        private int _dexterity;
+        public int Dexterity
+        {
+            get { return _dexterity; }
+            set { _dexterity = value; }
+        }
+
+        private int _constitution;
+        public int Constitution
+        {
+            get { return _constitution; }
+            set { _constitution = value; }
+        }
+
+        private int _intelligence;
+
+        public int Intelligence
+        {
+            get { return _intelligence; }
+            set { _intelligence = value; }
+        }
+
+        private int _wisdom;
+        public int Wisdom
+        {
+            get { return _wisdom; }
+            set { _wisdom = value; }
+        }
+
+        private int _charisma;
+        public int Charisma
+        {
+            get { return _charisma; }
+            set { _charisma = value; }
+        }
+    }
+}
