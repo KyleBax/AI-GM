0fc73723101bb8698f210fecb145bcd6b2e35c4c
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-06-03 10:57:23 +1200

    added more to species

--- Species.cs
+++ Species.cs
@@ -9,6 +9,41 @@
             set { _name = value; }
         }
 
+        private int _size;
+        public int Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+
+        private List<string> _languages;
+        public List<string> Languages
+        {
+            get { return _languages; }
+            set { _languages = value; }
+        }
+
+        private int _darkvisionRange;
+        public int DarkvisionRange
+        {
+            get { return _darkvisionRange; }
+            set { _darkvisionRange = value; }
+        }
+
+        private string _proficiency;
+        public string Prociciency
+        {
+            get { return _proficiency; }
+            set { _proficiency = value; }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
 
     }
 }
