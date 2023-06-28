d12f567be3a631bc998e6a4b34a04ca76ed538a8
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-06-28 13:19:41 +1200

    #6 dice rolls added

---
+++ Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_GM
+{
+    internal class Dice
+    {
+        public static int DiceRoll(int diceType)
+        {
+            Random random = new Random();
+            int diceRoll = random.Next(1, diceType + 1);
+            return diceRoll;    
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Character character = new Character();
 
             string newCharacter = UI.LoadCampaign();
-            if(newCharacter == "y")
+            if (newCharacter == "y")
             {
                 character = CharacterCreation.NewCharacter();
                 Logic.SerializeCharacter(character);
@@ -16,6 +16,22 @@
             {
                 character = Logic.DeserializeCharacter();
                 CharacterCreationUI.CharacterComplete(character, false);
+            }
+            while (true)
+            {
+                Console.WriteLine("press d followed by the number you want to roll. example d8");
+                string input = Console.ReadLine().ToLower();
+                int diceRoll = -1;
+
+                if (input.StartsWith("d") && input.Length > 1 && int.TryParse(input.Substring(1), out int diceType))
+                {
+                    diceRoll = Dice.DiceRoll(diceType);
+                    Console.WriteLine(diceRoll);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
             }
 
 
