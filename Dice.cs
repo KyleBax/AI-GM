c43f310e95e27974669c4c5e634ccaab567750dc
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-07-04 13:24:44 +1200

    random is now initialised at launch instead of in the  DiceRoll method

--- Dice.cs
+++ Dice.cs
@@ -8,9 +8,9 @@ namespace AI_GM
 {
     internal class Dice
     {
-        public static int DiceRoll(int diceType)
+        public static int DiceRoll(int diceType, Random random)
         {
-            Random random = new Random();
+
             int diceRoll = random.Next(1, diceType + 1);
             return diceRoll;    
         }
--- UI.cs
+++ UI.cs
@@ -8,5 +8,12 @@
             string input = Console.ReadLine().ToLower();
             return input;
         }
+
+        public static string GetInput()
+        {
+            Console.WriteLine("press d followed by the number you want to roll. example d8");
+            string input = Console.ReadLine().ToLower();
+            return input;
+        }
     }
 }
