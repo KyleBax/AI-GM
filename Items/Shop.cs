a8a45404b865a953d8f01f9fc8ce16ffdb2eee75
KyleBax <102880750+KyleBax@users.noreply.github.com>
2024-06-06 12:26:01 +1200

    #84 shop now has a way to sell junk items

--- Items/Shop.cs
+++ Items/Shop.cs
@@ -1,4 +1,5 @@
 ﻿using AI_GM.Characters;
+using AI_GM.UserInterface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,38 +13,72 @@ namespace AI_GM.Items
     {
         public static Character EnterShop(Character character)
         {
+            int shopSaleIncrease = 2;
             Item selectedItem = new Item();
             List<Item> instock = new List<Item>();
+            Console.WriteLine();
+            bool sell = UI.GetConfirmation("Would you like to sell your junk items?");
+            if (sell)
+            {
+                int saleCost = 0;
+                for (int i = 0; i < character.Inventory.Count; i++)
+                {
+                    saleCost += character.Inventory[i].Cost;
+                }
+                Console.WriteLine($"You have sold your junk for {saleCost} coins");
+                character.Inventory.Clear();
+                character.Coins += saleCost;
+            }
+
             for (int i = 0; i < 5; i++)
             {
                 instock.Add(Loot.GetRandomShopItem(i));
             }
             for (int i = 0; i < instock.Count; i++)
             {
-            Console.WriteLine($"item {i} Name: {instock[i].Name}, Type: {instock[i].Type}, Bonus Dice: {instock[i].ExtraDice}, Rarity: {instock[i].Rarity}, Cost: {instock[i].Cost *2}");
+                
+                Console.WriteLine($"item {i + 1} Name: {instock[i].Name}, Type: {instock[i].Type}, Bonus Dice: {instock[i].ExtraDice}, Rarity: {instock[i].Rarity}, Cost: {instock[i].Cost * shopSaleIncrease}");
             }
-            Console.WriteLine($"You have {character.Coins} coins");
-            Console.WriteLine("Would you like to buy anything?");
-            try
+            if (character.Coins >= 0)
             {
-                int selection = int.Parse(Console.ReadLine());
+                Console.WriteLine($"You have {character.Coins} coins");
+                Console.WriteLine("Would you like to buy anything?");
+                try
+                {
+                    int selection = int.Parse(Console.ReadLine());
 
-                if (selection >= 1 && selection <= instock.Count)
-                {
-                    selectedItem = instock[selection - 1];
+                    if (selection >= 1 && selection <= instock.Count)
+                    {
+                        selectedItem = instock[selection - 1];
+                        if (character.Coins >= selectedItem.Cost * shopSaleIncrease)
+                        {
+                            character.Coins = character.Coins - selectedItem.Cost * shopSaleIncrease;
+                            Console.WriteLine($"you have purchased {selectedItem.Name}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lack coins, return when you can afford something");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Selection");
+                        Console.WriteLine("Select one of these options");
+                    }
                 }
-                else
+                catch
                 {
-                    Console.WriteLine("Invalid Selection");
-                    Console.WriteLine("Select one of these options");
+                    Console.WriteLine("Please enter a valid number");
                 }
             }
-            catch
+            else
             {
-                Console.WriteLine("Please enter a valid number");
+                Console.WriteLine("You lack coins, return when you can afford something");
             }
 
-            
+
+
 
             return character;
         }
